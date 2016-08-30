bd213ff7bd7afc70fc6deaccc56865c9f113012f
Eric Hewitt <eric@understoryweather.com>
2016-08-30 15:07:31 -0400

    Fixed package creation

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 
 setup(name='topojson',
-    version='0.2.0',
+    version='0.2.1',
     description='An extension to GeoJSON that encodes topology.',
     url='https://github.com/osgn/python-topojson',
     download_url='https://github.com/osgn/python-topojson/archive/0.2.0.tar.gz',
     
-    packages=['topojson', 'geojson'],
+    packages=['topojson'],
     package_dir={
-      'topojson': 'src/topojson',
-      'geojson': 'src/geojson',
+      'topojson': 'src/'
     },
     
     license='BSD',
