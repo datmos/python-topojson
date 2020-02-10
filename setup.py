073e757ca94de7e407581fb7803afdbbee4f1e4a
Eric Hewitt <meandthewallaby@gmail.com>
2020-02-10 14:47:21 -0500

    chore: Bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='topojson',
-    version='0.2.1',
+    version='0.3.1',
     description='An extension to GeoJSON that encodes topology.',
     url='https://github.com/osgn/python-topojson',
     download_url='https://github.com/osgn/python-topojson/archive/0.2.0.tar.gz',
